141fa43b02d868a4d5439eafe833ddbf5ae44439
Rajamohan4487 <rajamohan4487@gmail.com>
2026-04-16 13:39:16 +0530

    roboshop Infra

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+
 growpart /dev/nvme0n1 4
 lvextend -r -L +30G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
 
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
