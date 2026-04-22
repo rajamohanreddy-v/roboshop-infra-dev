5cc8534e8c326402825f44470f9ebd090b693a08
Rajamohan4487 <rajamohan4487@gmail.com>
2026-04-22 11:28:26 +0530

    roboshop infra

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
+# We are creating 50GB root disk, but only 20GB is partitioned
+# Remaining 30GB we need to extend using below commands
 growpart /dev/nvme0n1 4
-pvresize /dev/nvme0n1p4
 lvextend -r -L +30G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
 
@@ -10,13 +10,9 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
 
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install terraform
-
 # creating databases
 cd /home/ec2-user
-git clone https://github.com/rajamohanreddy-v/roboshop-infra-dev.git
+git clone https://github.com/daws-88s/roboshop-infra-dev.git
 chown ec2-user:ec2-user -R roboshop-infra-dev
 cd roboshop-infra-dev/40-databases
 terraform init
@@ -24,6 +20,8 @@ terraform apply -auto-approve
 
 # creating components
 cd /home/ec2-user
+git clone https://github.com/daws-88s/roboshop-infra-dev.git
+chown ec2-user:ec2-user -R roboshop-infra-dev
 cd roboshop-infra-dev/90-components
 terraform init
 terraform apply -auto-approve
