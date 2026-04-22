a38d9e8dc60da795b483d512e467a31cf9419d83
Rajamohan4487 <rajamohan4487@gmail.com>
2026-04-22 11:58:31 +0530

    roboshop infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/daws-rss/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/rajamohanreddy-v/terraform-roboshop-component.git"
     component = each.key
     rule_priority = each.value.rule_priority
 }
