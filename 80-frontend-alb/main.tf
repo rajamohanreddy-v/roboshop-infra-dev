ffe025223168c3c0b909faf2d2b1bb799fce52a5
Rajamohan4487 <rajamohan4487@gmail.com>
2026-04-24 19:52:55 +0530

    roboshop infra

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -47,39 +47,3 @@ resource "aws_route53_record" "www" {
   }
   allow_overwrite = true
 }
-
-resource "aws_lb_target_group" "frontend" {
-  name     = "${var.project}-${var.environment}-frontend"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60
-
-  health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = "/"
-    port = 80
-    protocol = "HTTP"
-    timeout = 2
-    unhealthy_threshold = 3
-  }
-}
-resource "aws_lb_listener_rule" "frontend" {
-  # This is the resource name of the listener defined earlier in the same file
-  listener_arn = aws_lb_listener.https.arn 
-  priority     = 10
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend.arn
-  }
-
-  condition {
-    host_header {
-      # Make sure this matches the URL in your browser exactly
-      values = ["${var.project}-${var.environment}.${var.domain_name}"]
-    }
-  }
-}
